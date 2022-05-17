9e6b5a8e39e77d8e98ded24a9e85f75651c79c96
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-05-17 17:42:43 -0500

    otra vez

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,26 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma {
+    private String[] letras;
+    private String interpretacion;
+    
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+
+    public String interpretacion() {
+        return this.interpretacion;
+    } 
+
+    public String toString() {
+		String r = String.join(", ", this.letras);
+        return r;
+    }
+
+    public int cantidadLetras() {
+        return this.letras.length;
+    }
+
+}
